0966e9d6d8fccf403f99db20b93a918bf7b0864a
Titov Srgey <s.titov@ossystem.ua>
2020-04-21 18:20:21 +0300

    2020, apr 21 / 2nd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Car from './Car/Car';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cars: [
+      {name: 'Ford', year: '2005'},
+      {name: 'Audi', year: '2006'},
+      {name: 'Mazda', year: '2007'},
+      {name: 'Honda', year: '2008'}
+      {name: 'BMW', year: '2018'}
+    ],
+    pageTitle: 'part - 3',
+    showCars: false
+
+  };
+
+  changeShowCars = () => {
+    console.log('tut');
+    this.setState({
+      showCars: !this.state.showCars
+    });
+  };
+
+  render () {
+
+    let cars = null;
+    if (this.state.showCars) {
+      cars = this.state.cars.map((current, index) => {
+        return (
+          <Car
+            key={index}
+            name={current.name}
+            year={current.year}
+            onClickTitle={()=>this.setState({pageTitle:current.name})}
+          />
+        );
+      });
+    }
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>{this.state.pageTitle}</h1>
+          <button onClick={this.changeShowCars}>Show cars</button>
+
+          {cars}
+
+          {/*{this.state.showCars*/}
+          {/*  ? this.state.cars.map((current, index) => {*/}
+          {/*    return(*/}
+          {/*      <Car*/}
+          {/*        key={index}*/}
+          {/*        name={current.name}*/}
+          {/*        year={current.year}*/}
+          {/*      />*/}
+          {/*      )*/}
+          {/*  })*/}
+          {/*  : null*/}
+          {/*}*/}
+
+
+        </div>
+      </div>
+    );
+
+  }
 }
 
 export default App;
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import classes from './Car.module.css'
+
 
 export default props => (
-  <div>
+  <div className={classes.carBlock}>
     <h3>Car name: { props.name }</h3>
     <p>Car Year: <strong>{props.year}</strong></p>
+    <button className={classes.btn} onClick={props.onClickTitle}>Change Title</button>
 
   </div>
 )
